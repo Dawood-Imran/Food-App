aa4104edf57a671550234f471c17d0ee3a8e1e2f
Dawood <m.dawoodimran01@gmail.com>
2024-10-16 01:14:42 +0500

    Custom Component and Props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,23 @@
 import { StyleSheet } from 'react-native';
-import { Image } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import FoodComponent from '@/components/FoodComponent';
 
-import Food from '../../assets/data/Food'
+import Food from '@/assets/data/Food';
+
+
+
 
 
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-
-<Image source = {{uri:Food[0].image}} 
-      style = {styles.image}
-      />
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.title}>{Food[0].name}</Text>
-      <Text style={styles.title}>{Food[0].price}</Text>
-      
-      
-      
+    <View  style = {styles.container}>
+      <FoodComponent food = {Food} />
+         
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -31,17 +25,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  image:{
-    width: 300,
-    aspectRatio: 2/1
-  }
-});
+})
+
+
---
+++ components/FoodComponent.tsx
@@ -0,0 +1,56 @@
+import { View, Text, Image } from "react-native"
+import { StyleSheet } from "react-native"
+import Food from "@/assets/data/Food"
+
+const  FoodComponent = ({food}) => {
+
+    return(
+    <View style={styles.container}>
+  <Image source = {{uri:food[0].image}} 
+        style = {styles.image}
+        />
+        
+        <Text style={styles.title}>{food[0].name}</Text>
+        <Text style={styles.title}>{food[0].price}</Text>
+  
+        <Image source = {{uri:food[1].image}} 
+        style = {styles.image}
+        />
+        
+        <Text style={styles.title}>{food[1].name}</Text>
+        <Text style={styles.title}>{food[1].price}</Text>
+  
+
+    </View>)
+  }
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white',
+      margin: 10
+    },
+    separator: {
+      marginVertical: 30,
+      height: 1,
+      width: '80%',
+    },
+    image:{
+      width: 300,
+      aspectRatio: 2/1
+    }
+  });
+  
+  export default FoodComponent
+
+
+
+
+
